52955ee598a866131ddb4e3ac71825469f8476f5
ammarasimeurus <148402457+ammarasimeurus@users.noreply.github.com>
2024-01-10 02:45:35 +0500

    Update main.tf

--- terra_assignment/main.tf
+++ terra_assignment/main.tf
@@ -13,7 +13,7 @@ terraform {
   backend "s3" {
     bucket         = "ammar-jenkins"
     key            = "terraform.tfstate"   # The name of your state file
-    region         = local.reg
+    region         = "us-west-2"
     encrypt        = false                  # Optionally enable encryption
   }
 }
