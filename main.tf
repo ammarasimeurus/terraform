68341c20bd5540f4260329bad1f61fb595733a88
ammarasimeurus <148402457+ammarasimeurus@users.noreply.github.com>
2024-01-10 05:42:24 +0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -19,47 +19,4 @@ terraform {
   }
 }
 
-module "my_vpc" {
-  source               = "./modules/vpc"
-  for_each             = var.vpc_config
-  region               = local.reg
-  public_subnets_cidr  = each.value.public_subnets_cidr
-  private_subnets_cidr = each.value.private_subnets_cidr
-  enable_nat_gateway   = each.value.enable_nat_gateway
-  vpc_cider            = each.value.vpc_cider
-  vpc_name             = "${local.owner}-vpc"
-  allowed_externals    = each.value.allowed_externals
-
-}
-
-module "rds" {
-  source              = "./modules/rds"
-  username            = local.dbname
-  password            = local.dbpass
-  instance_class      = "db.t2.micro"
-  engine              = "mysql"
-  publicly_accessible = true
-  sg_name             = "${local.owner}-db-sg"
-  public_subnet       = module.my_vpc.vpc_1.public_subnet_ids
-  vpc_sg_id           = module.my_vpc.vpc_1.vpc_sg_id
-  depends_on          = [module.my_vpc]
-  owner               = local.owner
-}
-
-module "ecs" {
-
-  source         = "./modules/ecs"
-  dbname         = local.dbname
-  dbpass         = local.dbpass
-  owner          = local.owner
-  endpoint       = module.rds.endpoint
-  vpc_sg_id      = module.my_vpc.vpc_1.vpc_sg_id
-  public_subnet  = module.my_vpc.vpc_1.public_subnet_ids
-  my_vpc         = module.my_vpc.vpc_1.vpc_id
-  private_subnet = module.my_vpc.vpc_1.private_subnet_ids
-  img            = "public.ecr.aws/y2a9o9h4/ammar-ecr:latest"
-  depends_on     = [module.rds]
-}
-
-
  
